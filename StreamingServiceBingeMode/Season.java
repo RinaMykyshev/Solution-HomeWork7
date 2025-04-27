702001c3c9d2284b8612986d549fc5d8258a902d
Ваше Имя <_email@example.com>
2025-04-28 02:16:10 +0500

    Add Episode and basic Season iterator

---
+++ StreamingServiceBingeMode/Season.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Season implements Iterable<Episode> {
+    private final List<Episode> episodes = new ArrayList<>();
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public EpisodeIterator createIterator() {
+        return new SeasonIterator(episodes);
+    }
+
+    public EpisodeIterator createReverseIterator() {
+        return new ReverseSeasonIterator(episodes);
+    }
+
+    public EpisodeIterator createShuffleIterator(long seed) {
+        return new ShuffleSeasonIterator(episodes, seed);
+    }
+
+    @Override
+    public Iterator<Episode> iterator() {
+        return episodes.iterator();
+    }
+
+    public List<Episode> getEpisodes() {
+        return Collections.unmodifiableList(episodes);
+    }
+}
